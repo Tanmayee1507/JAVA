6cb9a7710239fd0d4863dfb9b09ba645abdb1f1e
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-04-02 23:13:51 +0530

    Create array1.java

---
+++ array1.java
@@ -0,0 +1,18 @@
+//single dimensional array
+class array1
+{
+    public static void main(String Arg[])
+    {
+        int Arr1[] = {10,20,30,40};  //static way
+
+        int Arr2[] = new int[4];  // Dynamic way
+        Arr2[0] = 10;
+        Arr2[1] = 20;
+        Arr2[2] = 30;
+        Arr2[3] = 40;
+        
+        System.out.println("Length of Arr1 is : "+Arr1.length);
+        System.out.println("Length of Arr2 is : "+Arr2.length);
+
+    }
+}
