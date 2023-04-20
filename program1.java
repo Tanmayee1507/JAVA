2b7fac784e58d2d23696ea3d106de624bf3973cb
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-04-20 22:05:02 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,15 @@
+/*
+Function name: Jay GAnesh
+Author: Tanmayee Kadam
+Date:18th April 2023
+language :Java programming
+*/
+import java.util.*;
+
+class Demo
+{
+    public static void main(String Arg[])
+    {
+        System.out.println("Jay Ganesh");
+    }
+}
