d1196366fc64a5c10680a96ad0354c9aeab71a2b
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-04-20 22:05:20 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,20 @@
+/*
+Function name: Addition
+Author: Tanmayee Kadam
+Date:18th April 2023
+language :Java programming
+*/
+
+class program2
+{
+    public static void main(String a[])
+    {
+        int No1 = 10;
+        int No2 = 11;
+        int Ans = 0;
+
+        Ans = No1 + No2;
+
+        System.out.println("Addition is:"+Ans);
+    }
+}
