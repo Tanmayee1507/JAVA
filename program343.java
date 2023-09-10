75becea79952e33e54eea8d761e2649008db10e5
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-09-10 23:23:19 +0530

    Add files via upload

---
+++ program343.java
@@ -0,0 +1,29 @@
+//Accept array from user, accept elements and display
+//sirs program343
+//Approach1
+import java.util.*;
+
+class program343
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner (System.in);
+        System.out.println("Enter number of elements:");
+        int iSize = sobj.nextInt();
+
+        int Arr[] = new int[iSize];
+
+        System.out.println("Enter the elements:");
+        int iCnt = 0;
+        for(iCnt = 0; iCnt < iSize; iCnt ++)
+        {
+            Arr[iCnt] = sobj.nextInt();
+        }
+
+         System.out.println("Elements of array are:");
+        for(iCnt = 0; iCnt < iSize; iCnt ++)
+        {
+            System.out.println(Arr[iCnt]);
+        }
+    }
+}
