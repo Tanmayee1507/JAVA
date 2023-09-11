91c9701373db55c5a49b5a51b06a961438be048f
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-09-11 22:56:57 +0530

    Add files via upload

---
+++ program348.java
@@ -0,0 +1,70 @@
+//Accept array from user, accept elements and display, summatin
+//Approach2
+//sirs program348
+import java.util.*;
+
+class program348
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner (System.in);
+        System.out.println("Enter number of elements:");
+        int iSize = sobj.nextInt();
+
+        MyArray mobj = new MyArray(iSize);
+        mobj.Accept();
+        mobj.Display();
+        System.out.println("Summation of all elements:"+mobj.Summation());
+    }
+}
+
+class ArrayX
+{
+   protected int Arr[];
+
+   public ArrayX (int iSize)
+   {
+    Arr = new int [iSize];
+   }
+
+   protected void Accept()
+   {
+     Scanner sobj = new Scanner (System.in);
+
+    System.out.println("Enter the elements:");
+
+    for(int iCnt = 0; iCnt < Arr.length; iCnt ++)
+    {
+        Arr[iCnt] = sobj.nextInt();
+    }
+   }
+
+   protected void Display()
+   {
+        System.out.println("Elements of Array are:");
+    
+    for(int iCnt = 0; iCnt < Arr.length; iCnt ++)
+    {
+        System.out.print(Arr[iCnt]+"\t");
+    }
+    System.out.println();
+   }
+}
+
+class MyArray extends ArrayX
+{
+    public MyArray(int iSize)
+    {
+        super (iSize);
+    }
+
+    public int Summation()
+    {
+        int iSum = 0;
+        for(int iCnt = 0; iCnt< Arr.length; iCnt++)
+        {
+            iSum = iSum + Arr[iCnt];
+        }
+        return iSum;
+    }
+}
