e5a1f61ccd37318f828d318fac18ef74bd18e923
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-09-19 23:13:52 +0530

    Add files via upload

---
+++ program362.java
@@ -0,0 +1,28 @@
+//Accept string and  capital characters change to small
+// sirs 362
+import java.util.*;
+class program362
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = sobj.nextLine();
+
+        StringX obj = new StringX();
+
+        String sret = obj.RemoveWhiteSpace(name);
+        System.out.println("Result is:"+sret);
+       
+    }  
+}  
+
+class StringX
+{
+      public String RemoveWhiteSpace(String str)
+    {
+        return str.replaceAll("\\s","");
+    }
+
+}
