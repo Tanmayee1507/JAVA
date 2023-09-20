7c95e63e8436827376650160ec6ffe83e6c04786
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-09-20 21:56:34 +0530

    Add files via upload

---
+++ program370.java
@@ -0,0 +1,27 @@
+//remove whte spaces
+//sirs program370
+import java.util.*;
+class program370
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the String:");
+        String str = sobj.nextLine();
+
+        str = str.replaceAll("\\s+"," ");
+        str = str.trim();
+        
+        int iFrequency = 0;
+
+        for(int iCnt = 0; iCnt < str.length(); iCnt++)
+        {
+            if(str.charAt(iCnt) ==' ')
+            {
+                iFrequency++;
+            }
+        }
+        System.out.println("Number of words are:"+ (iFrequency+1));
+    }
+}
